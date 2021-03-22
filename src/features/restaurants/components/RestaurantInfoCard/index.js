33e54ad0d9d8a86ca4de719aab6c77097f1086f9
gustavobgt <gustavo_bgt97@hotmail.com>
2021-03-22 08:38:05 -0300

    Rating star view added. Restaurant status, Restaurant Open | Closed and restaurant category view added.

--- src/features/restaurants/components/RestaurantInfoCard/index.js
+++ src/features/restaurants/components/RestaurantInfoCard/index.js
@@ -5,16 +5,24 @@ import {
   RestaurantCard,
   RestaurantCardCover,
   Info,
-  Adress,
+  Section,
   Title,
+  Rating,
+  SectionEnd,
+  Spacer,
+  RestaurantStatus,
+  CategoryImage,
+  Adress,
 } from './styles';
 
 import star from '../../../../../assets/icons/star';
+import open from '../../../../../assets/icons/open';
 
 export const RestaurantInfoCard = () => {
   const restaurant = {
     name: 'Some Restaurant 1',
-    icon: 'ICON',
+    icon:
+      'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos: [
       'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
     ],
@@ -24,12 +32,37 @@ export const RestaurantInfoCard = () => {
     isClosedTemporarily: true,
   };
 
+  const ratingArray = Array.from(new Array(Math.floor(restaurant.rating)));
+
   return (
     <RestaurantCard>
       <RestaurantCardCover source={{uri: restaurant.photos[0]}} />
       <Info>
         <Title>{restaurant.name}</Title>
-        <SvgXml xml={star} width={20} height={20} />
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {restaurant.isClosedTemporarily && (
+              <RestaurantStatus variant="label">
+                CLOSED TEMPORARILY
+              </RestaurantStatus>
+            )}
+
+            <Spacer />
+
+            {restaurant.isOpenNow && (
+              <SvgXml xml={open} width={20} height={20} />
+            )}
+
+            <Spacer />
+
+            <CategoryImage source={{uri: restaurant.icon}} />
+          </SectionEnd>
+        </Section>
         <Adress>{restaurant.address}</Adress>
       </Info>
     </RestaurantCard>
--- src/features/restaurants/components/RestaurantInfoCard/styles.js
+++ src/features/restaurants/components/RestaurantInfoCard/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import {Card} from 'react-native-paper';
 
 export const RestaurantCard = styled(Card)`
@@ -15,13 +15,46 @@ export const Info = styled(View)`
   padding: ${props => props.theme.space[3]};
 `;
 
-export const Adress = styled(Text)`
-  font-family: ${props => props.theme.fonts.body};
-  font-size: ${props => props.theme.fontSizes.caption};
-`;
-
 export const Title = styled(Text)`
   font-family: ${props => props.theme.fonts.heading};
   font-size: ${props => props.theme.fontSizes.body};
   color: ${props => props.theme.colors.ui.primary};
+`;
+
+export const Section = styled(View)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Rating = styled(View)`
+  display: flex;
+  flex-direction: row;
+  padding-top: ${props => props.theme.space[2]};
+  padding-bottom: ${props => props.theme.space[2]};
+`;
+
+export const SectionEnd = styled(View)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Spacer = styled(View)`
+  padding-left: 16px;
+`;
+
+export const RestaurantStatus = styled(Text)`
+  color: red;
+`;
+
+export const CategoryImage = styled(Image)`
+  width: 15px;
+  height: 15px;
+`;
+
+export const Adress = styled(Text)`
+  font-family: ${props => props.theme.fonts.body};
+  font-size: ${props => props.theme.fontSizes.caption};
 `;
